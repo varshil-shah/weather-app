6bb5fdca4f1fb0812378187679e42969efcf2829
Varshil-Shah <varshilashah09@gmail.com>
2023-07-20 23:47:37 +0530

    Design a heading widget

---
+++ lib/widgets/header_widget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+import '../controller/global_controller.dart';
+
+class HeaderWidget extends StatefulWidget {
+  const HeaderWidget({super.key});
+
+  @override
+  State<HeaderWidget> createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
+  String city = "";
+  String date = DateFormat("yMMMMd").format(DateTime.now());
+  final GlobalController globalController = Get.put(
+    GlobalController(),
+    permanent: true,
+  );
+
+  @override
+  void initState() {
+    getAddress(
+      globalController.getLattitude().value,
+      globalController.getLongitude().value,
+    );
+    super.initState();
+  }
+
+  getAddress(lat, long) async {
+    List<Placemark> placemark = await placemarkFromCoordinates(lat, long);
+    Placemark place = placemark[0];
+    setState(() {
+      city = place.locality!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20),
+          alignment: Alignment.topLeft,
+          child: Text(
+            city,
+            style: GoogleFonts.poppins(
+              height: 2,
+              fontSize: 35,
+              fontWeight: FontWeight.w500,
+              color: const Color.fromARGB(255, 33, 37, 71),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20),
+          alignment: Alignment.topLeft,
+          child: Text(
+            date,
+            style: GoogleFonts.poppins(
+              height: 1.5,
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: const Color.fromARGB(150, 33, 37, 71),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
