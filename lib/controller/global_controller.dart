e12485a5176c037ca48dac98faebaa04d732e6e1
Varshil-Shah <varshilashah09@gmail.com>
2023-07-21 20:36:21 +0530

    Add try-catch handler for location denied

--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -30,46 +30,46 @@ class GlobalController extends GetxController {
 
   getLocation() async {
     LocationPermission locationPermission;
-    bool isServiceEnable;
     _isLoading.value = true;
 
-    isServiceEnable = await Geolocator.isLocationServiceEnabled();
+    try {
+      // Check the status of permission
+      locationPermission = await Geolocator.checkPermission();
 
-    // Return if service is not enabled
-    if (!isServiceEnable) {
-      return Future.error("Location is not enabled");
-    }
+      if (locationPermission == LocationPermission.deniedForever) {
+        return Future.error("Location permission is denied forever!");
+      } else if (locationPermission == LocationPermission.denied) {
+        // Ask for permission again
+        locationPermission = await Geolocator.requestPermission();
 
-    // Check the status of permission
-    locationPermission = await Geolocator.checkPermission();
-
-    if (locationPermission == LocationPermission.deniedForever) {
-      return Future.error("Location permission is denied forever!");
-    } else if (locationPermission == LocationPermission.denied) {
-      // Ask for permission again
-      locationPermission = await Geolocator.requestPermission();
-
-      if (locationPermission == LocationPermission.denied) {
-        return Future.error("Location permission is denied!");
+        if (locationPermission == LocationPermission.denied) {
+          return Future.error("Location permission is denied!");
+        }
       }
-    }
 
-    // get current position
-    return await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.bestForNavigation,
-    ).then((value) {
-      // update latitude & longitude
-      _lattitude.value = value.latitude;
-      _longitude.value = value.longitude;
+      // get current position
+      return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation,
+      ).then((value) {
+        // update latitude & longitude
+        _lattitude.value = value.latitude;
+        _longitude.value = value.longitude;
 
-      // Make an API call
-      return FetchWeatherAPI()
-          .processData(value.latitude, value.longitude)
-          .then((value) {
-        weatherData.value = value;
-        _isLoading.value = false;
+        // Make an API call
+        return FetchWeatherAPI()
+            .processData(value.latitude, value.longitude)
+            .then((value) {
+          weatherData.value = value;
+          _isLoading.value = false;
+        });
       });
-    });
+    } catch (e) {
+      Get.snackbar(
+        'Location service',
+        'Location is denied or not turned on',
+        duration: const Duration(seconds: 5),
+      );
+    }
   }
 
   RxInt getIndex() {
