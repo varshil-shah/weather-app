e819699bbc8de4212a2d24dd15626ad96b088fd1
Varshil-Shah <varshilashah09@gmail.com>
2023-07-22 07:46:58 +0530

    Solve location permission access issue

--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:weather_app/api/fetch_weather.dart';
@@ -19,57 +21,73 @@ class GlobalController extends GetxController {
   WeatherData getWeatherData() => weatherData.value;
 
   @override
-  void onInit() {
+  void onInit() async {
+    super.onInit();
     if (_isLoading.isTrue) {
       getLocation();
+
+      // Timer to rerun app if location is turn on in between
+      bool isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!isServiceEnabled) {
+        Timer.periodic(
+          const Duration(seconds: 10),
+          (timer) async {
+            isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+            if (isServiceEnabled) {
+              getLocation();
+              timer.cancel();
+              print("Timer destroyed");
+            }
+          },
+        );
+      }
     } else {
       getIndex();
     }
-    super.onInit();
   }
 
   getLocation() async {
     LocationPermission locationPermission;
-    _isLoading.value = true;
 
-    try {
-      // Check the status of permission
-      locationPermission = await Geolocator.checkPermission();
+    // Check the status of permission
+    locationPermission = await Geolocator.checkPermission();
 
-      if (locationPermission == LocationPermission.deniedForever) {
-        return Future.error("Location permission is denied forever!");
-      } else if (locationPermission == LocationPermission.denied) {
-        // Ask for permission again
-        locationPermission = await Geolocator.requestPermission();
+    if (locationPermission == LocationPermission.deniedForever) {
+      return Future.error("Location permission is denied forever!");
+    } else if (locationPermission == LocationPermission.denied) {
+      // Ask for permission again
+      locationPermission = await Geolocator.requestPermission();
 
-        if (locationPermission == LocationPermission.denied) {
-          return Future.error("Location permission is denied!");
-        }
+      if (locationPermission == LocationPermission.denied) {
+        return Future.error("Location permission is denied!");
       }
+    }
 
-      // get current position
-      return await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.bestForNavigation,
-      ).then((value) {
-        // update latitude & longitude
-        _lattitude.value = value.latitude;
-        _longitude.value = value.longitude;
-
-        // Make an API call
-        return FetchWeatherAPI()
-            .processData(value.latitude, value.longitude)
-            .then((value) {
-          weatherData.value = value;
-          _isLoading.value = false;
-        });
-      });
-    } catch (e) {
-      Get.snackbar(
+    final isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isServiceEnabled) {
+      return Get.snackbar(
         'Location service',
         'Location is denied or not turned on',
         duration: const Duration(seconds: 5),
       );
     }
+
+    // get current position
+    return await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.bestForNavigation,
+    ).then((value) {
+      // update latitude & longitude
+      _lattitude.value = value.latitude;
+      _longitude.value = value.longitude;
+
+      // Make an API call
+      return FetchWeatherAPI()
+          .processData(value.latitude, value.longitude)
+          .then((value) {
+        weatherData.value = value!;
+        _isLoading.value = false;
+      });
+    });
   }
 
   RxInt getIndex() {
