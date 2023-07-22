2f29b882f9f52fe639cbd15c0107d3becaa2388c
Varshil-Shah <varshilashah09@gmail.com>
2023-07-22 07:45:02 +0530

    Add multiple api-keys

--- lib/api/fetch_weather.dart
+++ lib/api/fetch_weather.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_app/api/api_key.dart';
 import 'package:weather_app/model/weather_daily.dart';
 
 import 'package:weather_app/model/weather_data.dart';
@@ -11,16 +14,31 @@ import '../utils/api_url.dart';
 
 class FetchWeatherAPI {
   WeatherData? weather;
+  final RxBool _isApiKeysValid = false.obs;
 
   // processing the data from json => toJSON
-  Future<WeatherData> processData(lat, long) async {
-    var response = await http.get(Uri.parse(apiUrl(lat, long)));
-    var jsonString = jsonDecode(response.body);
-    weather = WeatherData(
-      WeatherCurrent.fromJSON(jsonString),
-      WeatherHourly.fromJSON(jsonString),
-      WeatherDaily.fromJSON(jsonString),
-    );
-    return weather!;
+  Future<WeatherData?> processData(lat, long) async {
+    for (var apiKey in apiKeys) {
+      var response = await http.get(Uri.parse(apiUrl(lat, long, apiKey)));
+      if (response.statusCode != 200) continue;
+      var jsonString = jsonDecode(response.body);
+      weather = WeatherData(
+        WeatherCurrent.fromJSON(jsonString),
+        WeatherHourly.fromJSON(jsonString),
+        WeatherDaily.fromJSON(jsonString),
+      );
+      _isApiKeysValid.value = true;
+      debugPrint("API_KEY: $apiKey");
+      return weather!;
+    }
+
+    if (_isApiKeysValid.isFalse) {
+      Get.snackbar(
+        'API',
+        'API is currently work not working\nPlease try again later!',
+        duration: const Duration(seconds: 5),
+      );
+    }
+    return null;
   }
 }
